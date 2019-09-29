9898527908d9b3c62b7968239586e7c1b86dfdb4
alifudinfm <alifudinfajar@gmail.com>
2019-09-29 19:06:34 +0700

    Penambahan file

---
+++ assets/js/todos.js
@@ -0,0 +1,30 @@
+
+$('#semangat').append(
+  "<audio controls autoplay hidden> <source src='../css/semangat.mp3' type='audio/mpeg' /></audio>"
+);
+// lengkapi jquery untuk menlist hasil dari todolist yang di inputkan
+$('ul').on('click', 'li', function() {
+  //  code Here...
+    if ($(this).css('color') === 'rgb(128, 128, 128)') {
+    $(this).css({
+      color: 'black',
+      textDecoration: 'none'
+    });
+  } else {
+    $(this).css({
+      color: 'gray',
+      textDecoration: 'line-through'
+    });
+  }
+});
+
+// buatlah jquery untuk menekan tombol akan menjadikan data sebagai list
+//  code Here...
+$(document).ready(function(){
+  $('#plus').click(function(){
+    var n = $('#item1').val();
+    $("#todos").append('<li><span><i class="fa fa-trash"></i></span> '+n+'</li>');
+    //untuk mencoret yang diklik
+  });  
+})
+
